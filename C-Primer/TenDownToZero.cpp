f847aa83200090b3132ddd9b1660c0acd87986d7
Xuechi Li <xli@algomau.ca>
2017-03-15 14:11:33 +0800

    Create TenDownToZero.cpp

---
+++ C-Primer/TenDownToZero.cpp
@@ -0,0 +1,13 @@
+/**
+   Use the decrement operator(--) to write a while that prints the numbers from ten down to zero.
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num = 11;
+	while(num--)
+		cout << num << endl;
+
+	return 0;
+} 
