09fcd08c6b70b3daf95d90b062ebf15d7987d2d9
Xuechi Li <xli@algomau.ca>
2017-03-26 21:17:11 +0800

    Create HelloWorldScene.h

---
+++ MagnetQuest/HelloWorldScene.h
@@ -0,0 +1,54 @@
+#ifndef __HELLOWORLD_SCENE_H__
+#define __HELLOWORLD_SCENE_H__
+
+#include "cocos2d.h"
+
+#include "SimpleAudioEngine.h"
+
+class HelloWorld : public cocos2d::CCLayerColor
+{
+public:
+	HelloWorld();
+	~HelloWorld();
+
+	// Here's a difference. Method 'init' in cocos2d-x returns bool, 
+    // instead of returning 'id' in cocos2d-iphone
+	virtual bool init();  
+
+	// there's no 'id' in cpp, so we recommand to return the exactly class pointer
+	static cocos2d::CCScene* scene();
+
+	// a selector callback
+	virtual void menuCloseCallback(cocos2d::CCObject* pSender);
+
+	// implement the "static node()" method manually
+	CREATE_FUNC(HelloWorld);
+
+	void spriteMoveFinished(cocos2d::CCNode* sender);
+
+	void playerMoveFinished();
+
+	void gameLogic(float dt);
+
+	void updateGame(float dt);
+
+	void registerWithTouchDispatcher();
+	void ccTouchesBegan(cocos2d::CCSet* touches, cocos2d::CCEvent* event);
+	void ccTouchesEnded(cocos2d::CCSet* touches, cocos2d::CCEvent* event);
+    
+    
+protected:
+	cocos2d::CCArray *_targets;
+	cocos2d::CCArray *_stars;
+	cocos2d::CCArray *_waterdrops;
+	cocos2d::CCArray *_projectiles;
+	int _projectilesDestroyed;
+
+	void addTarget();
+	void addStar();
+	void addWaterdrop();
+private:
+	cocos2d::CCSprite *m_pSprite;
+};
+
+#endif  // __HELLOWORLD_SCENE_H__
