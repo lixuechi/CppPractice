f50da8e10f41df423a78466325f301d5b073e00e
Xuechi Li <xli@algomau.ca>
2017-03-29 20:21:14 +0800

    - optm the algorithm
    still having time limit exceeded issue with really large numbers

--- leetcode/UglyNumber.cpp
+++ leetcode/UglyNumber.cpp
@@ -8,14 +8,10 @@
 using namespace std;
 
 bool isUgly(int num) {
-	if(num == 1) return true; 
+	if(num <= 0) return false;
 
-	vector<int> primeFactors;
-	bool isNumPrime = true;
-	for(int i = 2; i < num; i++) {
+	for(int i = 2; i <= num; i++) {
 		if(num % i == 0) { // is factor
-			isNumPrime = false;
-			// determine whether the factor is prime
 			bool isFactorPrime = true;
 			for(int j = 2; j < i; j++) {
 				if(i % j == 0) {
@@ -23,39 +19,30 @@ bool isUgly(int num) {
 					break;
 				} 
 			}
-			if(isFactorPrime) primeFactors.push_back(i);
+			if(isFactorPrime) {
+				if(i != 2 && i != 3 && i != 5)  return false;
+			}
 		}
 	}
-
-	if(isNumPrime) {
-		if(num != 2 && num != 3 && num != 5)
-			return false;
-		else
-			return true;
-	}
-
-	bool isInclude235 = false;
-	for(vector<int>::iterator it = primeFactors.begin(); it != primeFactors.end(); it++) {
-		cout << ' ' << *it;
-		
-		if(*it == 2 || *it == 3 || *it == 5)
-			isInclude235 = true;
-		else
-			return false;
-			
-	}
-	return isInclude235;
+	return true;
 }
 
 int main() {
-	//cout << isUgly(1) << endl;
+	///*
+	cout << isUgly(1) << endl;
 	cout << isUgly(2) << endl;
-	//cout << isUgly(3) << endl;
-	//cout << isUgly(5) << endl;
-	//cout << isUgly(6) << endl;
-	//cout << isUgly(8) << endl;
-	//cout << isUgly(10) << endl;
-	//cout << isUgly(14) << endl;
+	cout << isUgly(3) << endl;
+	cout << isUgly(5) << endl;
+	cout << isUgly(6) << endl;
+	//*/
+	cout << isUgly(7) << endl;
+	///*
+	cout << isUgly(8) << endl;
+	cout << isUgly(10) << endl;
+	cout << isUgly(14) << endl;
+	cout << isUgly(-2147483648) << endl;
+	cout << isUgly(905391974) << endl;
+	//*/
 
 	return 0;
 }
